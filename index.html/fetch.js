6be0027cb606c4d4010f2f34a523dfdb0ef22e74
coder-utshab <ghoshutshob470@gmail.com>
2026-02-03 16:38:08 +0530

    up

--- index.html/fetch.js
+++ index.html/fetch.js
@@ -1,1 +1,12 @@
-console.log('utshab')
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+function loadData() {
+    const url ='https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+
